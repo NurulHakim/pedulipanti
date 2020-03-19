c47fdb2ee4117508567df4ee6d9fac0b3ba7cecd
NurulHakim <nurulhakim2810@gmail.com>
2020-03-19 23:32:10 +0700

    updated

--- resources/views/body/landingpage.blade.php
+++ resources/views/body/landingpage.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.6">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" <meta name="generator"
+        content="Jekyll v3.8.6">
     <title>Peduli Panti</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/carousel/">
@@ -41,6 +41,16 @@
                 font-size: 3.5rem;
             }
         }
+
+        <style>
+
+        /* Make the image fully responsive */
+        .carousel-inner img {
+            width: 100%;
+            height: 100%;
+        }
+
+    </style>
 
     </style>
     <!-- Custom styles for this template -->
@@ -85,24 +95,19 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
-                        preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
-                        <rect width="100%" height="100%" fill="#777" /></svg>
+                    <img src="{{ asset('img/slidekesatu.png') }}" />
                     <div class="container">
                         <div class="carousel-caption text-left">
-                            <h1>Example headline.</h1>
-                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta
-                                gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                            <h1>pedulipanti</h1>
+                            <p>Mari berdayakan panti bersama pedulipanti.</p>
                             <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
-                        preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
-                        <rect width="100%" height="100%" fill="#777" /></svg>
+                    <img src="{{ asset('img/slidekedua.png') }}" />
                     <div class="container">
-                        <div class="carousel-caption">
+                        <div class="carousel-caption text-left">
                             <h1>Another example headline.</h1>
                             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta
                                 gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -111,11 +116,9 @@
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
-                        preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
-                        <rect width="100%" height="100%" fill="#777" /></svg>
+                    <img src="{{ asset('img/slideketiga.png') }}" />
                     <div class="container">
-                        <div class="carousel-caption text-right">
+                        <div class="carousel-caption text-left">
                             <h1>One more for good measure.</h1>
                             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta
                                 gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -144,42 +147,24 @@
             <!-- Three columns of text below the carousel -->
             <div class="row">
                 <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
-                        xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
-                        role="img" aria-label="Placeholder: 140x140">
-                        <title>Panti Asuhan TIARA PUTRI</title>
-                        <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777"
-                            dy=".3em">140x140</text>
-                    </svg>
-                    <h2>Panti Asuhan TIARA PUTRI</h2>
+                    <img src="{{ asset('img/tiaraputri.png') }}" />
+                    <h2>Panti Asuhan Tara Putri</h2>
                     <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh
                         ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
                         Praesent commodo cursus magna.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div><!-- /.col-lg-4 -->
                 <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
-                        xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
-                        role="img" aria-label="Placeholder: 140x140">
-                        <title>Rumah Yatim</title>
-                        <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777"
-                            dy=".3em">140x140</text>
-                    </svg>
+                    <img src="{{ asset('img/logo.png') }}" />
                     <h2>Rumah Yatim</h2>
                     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-                        Cras matt  is consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo,
+                        Cras matt is consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo,
                         tortor mauris condimentum nibh.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div><!-- /.col-lg-4 -->
                 <div class="col-lg-4">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140"
-                        xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
-                        role="img" aria-label="Placeholder: 140x140">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777"
-                            dy=".3em">140x140</text>
-                    </svg>
-                    <h2>Heading</h2>
+                    <img src="{{ asset('img/mahmudah.png') }}" />
+                    <h2>Panti Asuhan Mahmudah</h2>
                     <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id
                         ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris
                         condimentum nibh, ut fermentum massa justo sit amet risus.</p>
@@ -195,16 +180,11 @@
             <div class="row featurette">
                 <div class="col-md-7">
                     <h2 class="featurette-heading">Mari Beramal Ke Sesama</h2>
-                    <p class="lead">Mari Beramal Ke Panti-Panti Asuhan melalui PeduliPanti.id dan Raih Berkah dalam Hidupmu!</p>
+                    <p class="lead">Mari Beramal Ke Panti-Panti Asuhan melalui PeduliPanti.id dan Raih Berkah dalam
+                        Hidupmu!</p>
                 </div>
                 <div class="col-md-5">
-                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
-                        height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
-                        focusable="false" role="img" aria-label="Placeholder: 500x500">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa"
-                            dy=".3em">500x500</text>
-                    </svg>
+                    <img src="{{ asset('img/gambar1.png') }}" />
                 </div>
             </div>
 
@@ -216,13 +196,7 @@
                     <p class="lead"></p>
                 </div>
                 <div class="col-md-5 order-md-1">
-                    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
-                        height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
-                        focusable="false" role="img" aria-label="Placeholder: 500x500">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa"
-                            dy=".3em">500x500</text>
-                    </svg>
+                    <img src="{{ asset('img/gambar2.jpg') }}" />
                 </div>
             </div>
 
@@ -236,7 +210,7 @@
 
         <!-- FOOTER -->
         <footer class="container">
-            <div class="col-6 col-md-4">
+            <div class="col-6 col-md-4 ">
                 <h5 class="card-title">Berdayakan panti bersama pedulipanti</h5>
                 <p class="card-text">pedulipanti adalah layanan untuk perusahaan atau organisasi yang mencari sebuah
                     informasi panti asuhan.</p>
