97588fcd38058a42e879474f079e4320279cf594
kurniawanedi <koerniawanedi@gmail.com>
2020-04-14 19:55:28 +0700

    update routes\web.php

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/listpanti', 'PantiController@listview');
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
-// Route::get('/', 'HomeController@index')->name('home');
-Route::get('/', function () {
-    return view('body/landingpage');
-});
+Route::get('/', 'PantiController@viewpanti');
+// Route::get('/', function () {
+//     return view('body/landingpage');
+// });
