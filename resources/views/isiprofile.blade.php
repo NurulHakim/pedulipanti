6321888e323279b45471dbce59e0810fa583e596
kurniawanedi <koerniawanedi@gmail.com>
2020-04-22 20:28:30 +0700

    Update isiprofile.blade.php

--- resources/views/isiprofile.blade.php
+++ resources/views/isiprofile.blade.php
@@ -25,7 +25,7 @@
 
 <body>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href=""> PeduliPanti</a>
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/"> PeduliPanti</a>
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
                 <a class="nav-link" href="{{route('logout')}}" onclick="event.preventDefault();
