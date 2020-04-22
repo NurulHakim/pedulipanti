ff359e3c7fb40fa2dbc95a7b80029be9cc475146
kurniawanedi <koerniawanedi@gmail.com>
2020-04-23 00:56:02 +0700

    Update landingpage.blade.php

--- resources/views/body/landingpage.blade.php
+++ resources/views/body/landingpage.blade.php
@@ -73,9 +73,12 @@
                                 Dashboard
                             </a>
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
+                                document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
+                            <a class="dropdown-item" href="">
+                                Hapus Akun                                
+                            </a>   
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
