77270953e09eaec4667a2be2b035b613a718371d
kurniawanedi <koerniawanedi@gmail.com>
2020-04-25 13:58:51 +0700

    Update galerypanti.blade.php

--- resources/views/galerypanti.blade.php
+++ resources/views/galerypanti.blade.php
@@ -32,58 +32,10 @@
 
 </head>
 
-<body>
-  
-
-    <header>
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="/" style="margin-left: 1em"><b style="color: white">pedulipanti</b></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="navbar-collapse collapse " id="navbarSupportedContent" style="margin-right: 2em">
-                <ul class="navbar-nav ml-auto">
-
-                    <li class="nav-item" style="margin-right: 1em">
-                        <a class="nav-link" href="/listpanti">Cari Panti<span class="sr-only"></span></a>
-                    </li>
-                    <li class="nav-item" style="margin-right: 1em">
-                        <a class="nav-link" href="#">Tentang Kami</a>
-                    </li>
-                    @guest
-                    <li class="nav-item" style="margin-right: 1em">
-                        <a class="btn btn-outline-primary" href="{{route('login')}}" role="button" style="color: white; border-color: rgb(245, 121, 12)">Login</a>
-                    </li>
-                    @if (Route::has('register'))
-                    <li class="nav-item" style="margin-right: 1em">
-                        <a class="btn btn-primary" href="{{route('register')}}" role="button" style="background-color: rgb(245, 121, 12); border-color: rgb(245, 121, 12)">Sign Up</a>
-                    </li>
-                    @endif
-                    @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="/dashboard">
-                                Dashboard
-                            </a>
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                    @endguest
-                </ul>
-            </div>
-        </nav>
-    </header>
+<body>  
+    <!-- HEADER -->
+    @include('header.index')
+    <!-- END OF HEADER -->
 
     <main role="main">
 
@@ -110,16 +62,12 @@
         <p class="float-right">
             <a href="#">Back to top</a>
         </p>
+    </div>
+    <br>
 
-    </div> <br>
-    <footer class="page-footer font-small blue" style="padding-top: 1em; padding-bottom: 1em">
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© 2020 Copyright: v09042929
-        </div>
-        <!-- Copyright -->
-
-    </footer>
+    <!-- FOOTER -->
+    @include('footer.index')
+    <!-- END OF FOOTER -->
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -132,6 +80,40 @@
     <script src="../../assets/js/vendor/popper.min.js"></script>
     <script src="../../dist/js/bootstrap.min.js"></script>
     <script src="../../assets/js/vendor/holder.min.js"></script>
+
+    <!-- All JS Custom Plugins Link Here here -->
+    <script src="{{asset('js/modernizr-3.5.0.min.js')}}"></script>
+
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="{{asset('js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{asset('js/poppers.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="{{asset('js/jquery.slicknav.min.js')}}"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('js/slick.min.js')}}"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="{{asset('js/wow.min.js')}}"></script>
+    <script src="{{asset('js/animated.headline.js')}}"></script>
+    <script src="{{asset('js/jquery.magnific-popup.js')}}"></script>
+
+    <!-- Scrollup, nice-select, sticky -->
+    <script src="{{asset('js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{asset('js/jquery.sticky.js')}}"></script>
+
+    <!-- contact js -->
+    <script src="{{asset('js/contact.js')}}"></script>
+    <script src="{{asset('js/jquery.form.js')}}"></script>
+    <script src="{{asset('js/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('js/mail-script.js')}}"></script>
+    <script src="{{asset('js/jquery.ajaxchimp.min.js')}}"></script>
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="{{asset('js/plugins.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
 </body>
 
 </html>
