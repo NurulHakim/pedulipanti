683b66b92c3afe435c91350fffe0d1aafb8e52dc
kurniawanedi <koerniawanedi@gmail.com>
2020-04-25 15:15:18 +0700

    Delete index.blade.php

--- resources/views/footer/index.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="page-footer font-small blue" style="padding-top: 1em; padding-bottom: 1em">
-
-<!-- Copyright -->
-<div class="footer-copyright text-center py-3">© 2020 Copyright: v09042929
-</div>
-<!-- Copyright -->
-
-</footer>
